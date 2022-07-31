18709c2e1105a967d7adf36c7006fe4bb159b447
Josias <me@josias.dev>
2022-07-30 22:34:12 -0700

    Add window

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 build:
-	#g++ -o TETRIS `pkg-config --cflags --libs sdl2` main.cpp
 	g++ -o TETRIS main.cpp `pkg-config --cflags --libs sdl2`
--- main.cpp
+++ main.cpp
@@ -4,6 +4,33 @@
 
 int main()
 {
-	SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
-	return 0;
+	 if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(1000);
 }
