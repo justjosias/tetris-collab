2047aa568f85e3cb8dfb19d60f53c6600fb3c5ba
Josias <me@josias.dev>
2022-07-30 23:43:16 -0700

    Use namespace std

--- main.cpp
+++ main.cpp
@@ -4,26 +4,18 @@
 
 #include <SDL.h>
 
+using namespace std;
+
 class Game
 {
       public:
-	std::vector<std::string> blocks;
-
         int height;
         int width;
 
 	Game() {
                 this->height = 20;
                 this->width = 10;
-                this->blocks.push_back("l-shaped");
         }
-
-	void list_blocks()
-	{
-		for (std::string block : this->blocks) {
-			std::cout << block << std::endl;
-		}
-	}
 };
 
 int main()
@@ -32,23 +24,23 @@ int main()
 	game.list_blocks();
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cerr << "Failed to initialize the SDL2 library\n";
+		cerr << "Failed to initialize the SDL2 library\n";
 		return -1;
 	}
 
 	SDL_Window *window =
 	    SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED,
-			     SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+			     SDL_WINDOWPOS_CENTERED, 680, 480, 0)pp;
 
 	if (!window) {
-		std::cerr << "Failed to create window\n";
+		cerr << "Failed to create window\n";
 		return -1;
 	}
 
 	SDL_Surface *window_surface = SDL_GetWindowSurface(window);
 
 	if (!window_surface) {
-		std::cout << "Failed to get the surface from the window\n";
+		cerr << "Failed to get the surface from the window\n";
 		return -1;
 	}
 
