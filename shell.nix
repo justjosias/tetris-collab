14ceea4bfd69f3dd57370c4fdc91dfed1f7580fa
Josias <me@josias.dev>
2022-09-28 19:51:57 -0700

    Add shell.nix for nix development

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    nativeBuildInputs = [
+      pkgs.SDL2
+      pkgs.SDL2_ttf
+      pkgs.SDL2_image
+      pkgs.SDL2_mixer
+      pkgs.pkg-config
+    ];
+}
+
